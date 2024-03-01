e981539f466e3adf1f1d48cb17ab7726f9a9324c
Judoguy <judoguy79@gmail.com>
2024-03-01 21:17:32 +0900

    js file

---
+++ Day6/js-intro.js
@@ -0,0 +1,35 @@
+function sayHello(){
+    console.log("Hello, World!"); //statement
+}
+
+
+sayHello(); //camelCase
+
+
+// Comments
+// single-line comments
+
+/* 
+multi 
+line        shift + alt + a
+comment
+ */ 
+
+
+
+// Varibles
+// let
+let number = 18; //mutable variable
+number = 23;
+console.log(number);
+
+// const can not be changed
+const name = "Matt";
+// name = "Tim";
+console.log(name);
+
+// var
+var age = 20;
+age = 21;
+console.log(age);
+
